36661b667bba14d27edd2e9f122088e59b9414f6
Adam Magyar <magyaradam33@gmail.com>
2019-05-13 23:26:59 +0200

    Updates

---
+++ precache-manifest.dbf90032686d75a329bd8564efd0f318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de678a63088dc60a3624ac725637912",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf295085c91c96a60b2c",
+    "url": "/static/css/2.c489bcc4.chunk.css"
+  },
+  {
+    "revision": "e2af4f0953626aac9ce0",
+    "url": "/static/css/main.3f8c9fc8.chunk.css"
+  },
+  {
+    "revision": "cf295085c91c96a60b2c",
+    "url": "/static/js/2.b98ce261.chunk.js"
+  },
+  {
+    "revision": "e2af4f0953626aac9ce0",
+    "url": "/static/js/main.4e13a7aa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
